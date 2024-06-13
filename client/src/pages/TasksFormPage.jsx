96d77a9fe9bb3a8a08633fa622db28dbfcd5a23c
al3xHub <alejandrobueno92@hotmail.com>
2024-06-13 19:13:09 +0200

    delete tarea

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form';
-import { createTask } from '../api/tasks.api';
-import {useNavigate} from 'react-router-dom';
+import { createTask, deleteTask } from '../api/tasks.api';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export function TasksFormPage() {
 
@@ -9,6 +9,7 @@ export function TasksFormPage() {
     } } = useForm();
 
     const navigate = useNavigate();
+    const param = useParams();
 
     const onSubmit = handleSubmit(async data => {
         await createTask(data);
@@ -17,26 +18,35 @@ export function TasksFormPage() {
     })
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                type="text"
-                placeholder="title"
-                {...register("title", { required: true })}
-            />
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="title"
+                    {...register("title", { required: true })}
+                />
 
-            {errors.title && <span>This field is required</span>}
+                {errors.title && <span>This field is required</span>}
 
-            <textarea
-                name=""
-                id=""
-                rows="3"
-                placeholder="description"
-                {...register("description", { required: true })}
-            ></textarea>
+                <textarea
+                    name=""
+                    id=""
+                    rows="3"
+                    placeholder="description"
+                    {...register("description", { required: true })}
+                ></textarea>
 
-            {errors.description && <span>This field is required</span>}
+                {errors.description && <span>This field is required</span>}
 
-            <button>Save</button>
-        </form>
+                <button>Save</button>
+            </form>
+            {param.id && <button onClick={async () =>{
+                const accepted = window.confirm("Are you sure you want to delete this task?")
+                if(accepted){
+                    await deleteTask(param.id);
+                    navigate("/tasks");
+                }
+            }}>Delete</button>}
+        </div>
     )
 }
