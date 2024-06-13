84e67f2513cacbb5b5c6d7c9f6486ea7cf2f3a80
al3xHub <alejandrobueno92@hotmail.com>
2024-06-13 18:16:15 +0200

    creando tareas en cliente

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,5 +1,42 @@
-export function TasksFormPage(){
-    return(
-        <div>Tasks form page</div>
+import { useForm } from 'react-hook-form';
+import { createTask } from '../api/tasks.api';
+import {useNavigate} from 'react-router-dom';
+
+export function TasksFormPage() {
+
+    const { register, handleSubmit, formState: {
+        errors
+    } } = useForm();
+
+    const navigate = useNavigate();
+
+    const onSubmit = handleSubmit(async data => {
+        await createTask(data);
+        navigate("/tasks")
+
+    })
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="title"
+                {...register("title", { required: true })}
+            />
+
+            {errors.title && <span>This field is required</span>}
+
+            <textarea
+                name=""
+                id=""
+                rows="3"
+                placeholder="description"
+                {...register("description", { required: true })}
+            ></textarea>
+
+            {errors.description && <span>This field is required</span>}
+
+            <button>Save</button>
+        </form>
     )
 }
