d9f1bcfa88e433c2b1e1d38e1b50fba09b0b1172
al3xHub <alejandrobueno92@hotmail.com>
2024-07-09 11:14:44 +0200

    cambios

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     outDir: 'dist',
   },
 });
+
 
