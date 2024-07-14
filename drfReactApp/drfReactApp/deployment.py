de25190d9d570c5d6b545b33ce0c721ecba41038
al3xHub <alejandrobueno92@hotmail.com>
2024-07-14 13:59:53 +0200

    postgresql database for deploy

--- drfReactApp/drfReactApp/deployment.py
+++ drfReactApp/drfReactApp/deployment.py
@@ -30,12 +30,12 @@ STORAGES = {
     },
 }
 
-CONNECTION = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
+CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": CONNECTION_STR['dbname'],
         "HOST": CONNECTION_STR['host'],
         "USER": CONNECTION_STR['user'],
