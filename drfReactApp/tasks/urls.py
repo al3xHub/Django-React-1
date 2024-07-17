f3bb57f4dbc2b32070fadaba9c167b3458988927
al3xHub <alejandrobueno92@hotmail.com>
2024-07-17 09:52:18 +0200

    cambiando urls.py

--- drfReactApp/tasks/urls.py
+++ drfReactApp/tasks/urls.py
@@ -8,6 +8,6 @@ router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
+    path('', include(router.urls)),
     path('docs/', include_docs_urls(title='Task API'))
 ]
